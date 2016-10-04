e60b5e43d231d00ce5311bfed4b0a54f696b24af
Alexandre Odoni <alexandre.odoni@cmc.pr.gov.br>
2016-10-04 14:38:31 -0300

    relacionamento departamento/ramal

--- telefonia/admin.py
+++ telefonia/admin.py
@@ -4,7 +4,15 @@ from django.contrib import admin
 
 from .models import Departamento, Ramal, Problemas, Status, Urgencia, Executor, Chamado, TipoEquipamento, TrocaEquipamento, ProblemasComuns
 
-admin.site.register(Departamento)
+class RamalInline(admin.TabularInline):
+	model = Ramal
+	extra = 3
+
+class DepartamentoAdmin(admin.ModelAdmin):
+	list_display = ['nome']
+	inlines = [RamalInline]
+
+admin.site.register(Departamento, DepartamentoAdmin)
 admin.site.register(Ramal)
 admin.site.register(Problemas)
 admin.site.register(Status)
--- telefonia/models.py
+++ telefonia/models.py
@@ -14,6 +14,7 @@ class Departamento(models.Model):
 class Ramal(models.Model):
 	nome = models.CharField(max_length=200)
 	numero = models.CharField(max_length=20)
+	departamento = models.ForeignKey('Departamento')
 
 	def __unicode__(self):
 		return self.nome + ' - ' + self.numero
